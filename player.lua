a80758d6ef679047035ae6536accf68438ad6a1b
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-04-02 14:38:47 +0800

    better p3d

--- player.lua
+++ player.lua
@@ -3,10 +3,10 @@ local player = {dir = 0, x = 0, y = 0}
 local function p3d(p, rotation)
   rotation = rotation or player.dir
 
-  local x = math.sin(rotation)*p.x 		+	math.sin(rotation)*p.z
-  local y = math.cos(rotation)*p.x/2	+	math.cos(rotation)*p.z/2
+  local x = math.sin(rotation)*p.x 	 + 0   +	math.sin(rotation+math.pi/2)*p.z
+  local y = math.cos(rotation)*p.x/2 + p.y +	math.cos(rotation+math.pi/2)*p.z/2
 
-  local f = math.cos(rotation)*p.x 		+	math.cos(rotation)*p.z
+  local f = math.cos(rotation)*p.x 	 + p.y +	math.cos(rotation+math.pi/2)*p.z
 
   return x,y,z
 end
@@ -17,10 +17,10 @@ function player.draw()
 
 	--[[ Legs ]]
   love.graphics.setColour(0.8*0.8,0.6*0.8,0.3*0.8)
-  x,y,f = p3d({x=6, y=0, z=0}, player.dir+math.pi/2)
-  love.graphics.rectangle("fill", x-4, y+12, 8, 12)
-  x,y,f = p3d({x=6, y=0, z=0}, player.dir-math.pi/2)
-  love.graphics.rectangle("fill", x-4, y+12, 8, 12)
+  x,y,f = p3d({x=6, y=12, z=0}, player.dir+math.pi/2)
+  love.graphics.rectangle("fill", x-4, y, 8, 12)
+  x,y,f = p3d({x=6, y=12, z=0}, player.dir-math.pi/2)
+  love.graphics.rectangle("fill", x-4, y, 8, 12)
 
   --[[ Back Arm ]]
   love.graphics.setColour(0.3*0.9,0.6*0.9,0.8*0.9)
@@ -39,10 +39,10 @@ function player.draw()
 
   --[[ Back Eyes ]]
   love.graphics.setColour(0.13,0.13,0.13)
-  x,y,f = p3d({x=16, y=0, z=0}, (player.dir)-14/180*math.pi)
-  love.graphics.ellipse("fill", x, y-20, 2.5, 5)
-  x,y,f = p3d({x=16, y=0, z=0}, (player.dir)+14/180*math.pi)
-  love.graphics.ellipse("fill", x, y-20, 2.5, 5)
+  x,y,f = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
+  love.graphics.ellipse("fill", x, y, 2.5, 5)
+  x,y,f = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
+  love.graphics.ellipse("fill", x, y, 2.5, 5)
 
   --[[ Head ]]
   love.graphics.setColour(0.9,0.7,0.6)
@@ -51,12 +51,12 @@ function player.draw()
   --[[ Front Eyes ]]
   love.graphics.setColour(0.13,0.13,0.13)
   if player.dir-14/180*math.pi < math.pi/2 and player.dir-14/180*math.pi > -math.pi/2 then
-    x,y,f = p3d({x=16, y=0, z=0}, (player.dir)-14/180*math.pi)
-    love.graphics.ellipse("fill", x, y-20, 2.5, 5)
+    x,y,f = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
+    love.graphics.ellipse("fill", x, y, 2.5, 5)
   end
   if player.dir+14/180*math.pi < math.pi/2 and player.dir+14/180*math.pi > -math.pi/2 then
-    x,y,f = p3d({x=16, y=0, z=0}, (player.dir)+14/180*math.pi)
-    love.graphics.ellipse("fill", x, y-20, 2.5, 5)
+    x,y,f = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
+    love.graphics.ellipse("fill", x, y, 2.5, 5)
   end
   
   love.graphics.translate(-player.x, -(player.y/2-24))
