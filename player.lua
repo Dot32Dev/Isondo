4adac11098412d73d4c59942b11c01c4acd886d7
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-04-24 20:06:57 +0800

    automatic depth sorting!

--- player.lua
+++ player.lua
@@ -1,5 +1,5 @@
 intro = require('Intro') -- require intro for access to timer
-local player = {dir = 0, x = 0, y = 0, z = 0, xV = 0, yV = 0, zV = 0, animFrame = 0}
+local player = {dir = 0, x = 0, y = 0, z = 0, xV = 0, yV = 0, zV = 0, animFrame = 0, objects = {}}
 
 local function p3d(p, rotation)
   rotation = rotation or player.dir -- rotation is a scalar that rotates around the y axis
@@ -7,71 +7,105 @@ local function p3d(p, rotation)
   local x = math.sin(rotation)*p.x 	 + 0   +	math.sin(rotation+math.pi/2)*p.z
   local y = math.cos(rotation)*p.x/2 + p.y +	math.cos(rotation+math.pi/2)*p.z/2
 
-  local z = math.cos(rotation)*p.x 	 + p.y +	math.cos(rotation+math.pi/2)*p.z
+  local z = math.cos(rotation)*p.x 	 - p.y +	math.cos(rotation+math.pi/2)*p.z
 
   return x,y,z
 end
 
+local x,y,z, x1,y1,z1 = 0,0,0, 0,0,0 -- creates the local variables, outputs dont get used
+player.objects = { -- table of every body part in the player (automatically gets sorted by z each frame)
+  {
+    id='legs',
+    z=1,
+    draw = function(self)
+      love.graphics.setColour(0.8*0.8,0.6*0.8,0.3*0.8)
+      love.graphics.setLineWidth(8)
+      x,y,z = p3d({x=0, y=12, z=6})
+      x1,y1,z1 = p3d({x=math.sin(math.sin(player.animFrame)*math.pi/2)*12, y=12+math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=6})
+      love.graphics.line(x, y, x1, y1) -- left leg
+      x,y,z = p3d({x=0, y=12, z=-6})
+      x1,y1,z1 = p3d({x=math.sin(-math.sin(player.animFrame)*math.pi/2)*12, y=12+math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=-6})
+      love.graphics.line(x, y, x1, y1)
+
+      _, _, self.z = p3d({x=0, y=12, z=0})
+    end 
+  },
+  {
+    id='left arm',
+    z=2,
+    draw = function(self)
+      love.graphics.setColour(0.3*0.9,0.6*0.9,0.8*0.9)
+      x,y,z = p3d({x=0, y=0, z=20})
+      x1,y1,z1 = p3d({x=math.sin(-math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=20})
+      love.graphics.line(x, y, x1, y1)
+
+      _, _, self.z = p3d({x=0, y=0, z=20})
+    end 
+  },
+  {
+    id='right arm',
+    z=3,
+    draw = function(self)
+      love.graphics.setColour(0.3*0.9,0.6*0.9,0.8*0.9)
+      x,y,z = p3d({x=0, y=0, z=-20})
+      x1,y1,z1 = p3d({x=math.sin(math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=-20})
+      love.graphics.line(x, y, x1, y1)
+
+      _, _, self.z = p3d({x=0, y=0, z=-20})
+    end 
+  },
+  {
+    id='body',
+    z=4,
+    draw = function(self)
+      love.graphics.setColour(0.3,0.6,0.8)
+      love.graphics.ellipse("fill", 0, 0, 20, 19)
+
+      _, _, self.z = p3d({x=0, y=0, z=0})
+    end 
+  },
+  {
+    id='left eye',
+    z=5,
+    draw = function(self)
+      love.graphics.setColour(0.13,0.13,0.13)
+      x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
+      love.graphics.ellipse("fill", x, y, 2.5, 5)
+
+      _, _, self.z = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
+    end 
+  },
+  {
+    id='right eye',
+    z=6,
+    draw = function(self)
+      love.graphics.setColour(0.13,0.13,0.13)
+      x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
+      love.graphics.ellipse("fill", x, y, 2.5, 5)
+
+      _, _, self.z = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
+    end 
+  },
+  {
+    id='head',
+    z=7,
+    draw = function(self)
+      love.graphics.setColour(0.9,0.7,0.6)
+      love.graphics.ellipse("fill", 0, 0-20, 20/1.2, 19/1.2)
+
+      _, _, self.z = p3d({x=0, y=-20, z=0})
+    end 
+  }
+}
+
 function player.draw()
-	local x,y,z, x1,y1,z1 = 0,0,0, 0,0,0 -- creates the local variables, outputs dont get used
-	love.graphics.translate(player.x, player.z-24+player.y)
+	love.graphics.translate(player.x, player.z-24+player.y) -- 24 is the hight from the players feet to the body
 
-	--[[ Legs ]]
-  love.graphics.setColour(0.8*0.8,0.6*0.8,0.3*0.8)
-  love.graphics.setLineWidth(8)
-  x,y,z = p3d({x=0, y=12, z=6})
-  x1,y1,z1 = p3d({x=math.sin(math.sin(player.animFrame)*math.pi/2)*12, y=12+math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=6})
-  love.graphics.line(x, y, x1, y1) -- left leg
-  x,y,z = p3d({x=0, y=12, z=-6})
-  x1,y1,z1 = p3d({x=math.sin(-math.sin(player.animFrame)*math.pi/2)*12, y=12+math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=-6})
-  love.graphics.line(x, y, x1, y1) -- right leg
-
-  --[[ Back Arms ]]
-  love.graphics.setColour(0.3*0.9,0.6*0.9,0.8*0.9)
-  x,y,z = p3d({x=0, y=0, z=20})
-  x1,y1,z1 = p3d({x=math.sin(-math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=20})
-  love.graphics.line(x, y, x1, y1)
-  x,y,z = p3d({x=0, y=0, z=-20})
-  x1,y1,z1 = p3d({x=math.sin(math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=-20})
-  love.graphics.line(x, y, x1, y1)
- 
-  --[[ Body ]]
-  love.graphics.setColour(0.3,0.6,0.8)
-  love.graphics.ellipse("fill", 0, 0, 20, 19)
-
-  --[[ Front Arms ]]
-  love.graphics.setColour(0.3*0.9,0.6*0.9,0.8*0.9)
-  if player.dir+math.pi/2 < math.pi/2 and player.dir+math.pi/2 > -math.pi/2 then
-    x,y,z = p3d({x=0, y=0, z=20})
-    x1,y1,z1 = p3d({x=math.sin(-math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=20})
-    love.graphics.line(x, y, x1, y1)
-  end
-  if player.dir-math.pi/2 < math.pi/2 and player.dir-math.pi/2 > -math.pi/2 then
-    x,y,z = p3d({x=0, y=0, z=-20})
-    x1,y1,z1 = p3d({x=math.sin(math.sin(player.animFrame)*math.pi/2)*12, y=math.cos(math.sin(player.animFrame)*math.pi/2)*12, z=-20})
-    love.graphics.line(x, y, x1, y1)
-  end
-
-  --[[ Back Eyes ]]
-  love.graphics.setColour(0.13,0.13,0.13)
-  x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
-  love.graphics.ellipse("fill", x, y, 2.5, 5)
-  x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
-  love.graphics.ellipse("fill", x, y, 2.5, 5)
-
-  --[[ Head ]]
-  love.graphics.setColour(0.9,0.7,0.6)
-  love.graphics.ellipse("fill", 0, 0-20, 20/1.2, 19/1.2)
-
-  --[[ Front Eyes ]]
-  love.graphics.setColour(0.13,0.13,0.13)
-  if player.dir-14/180*math.pi < math.pi/2 and player.dir-14/180*math.pi > -math.pi/2 then
-    x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)-14/180*math.pi)
-    love.graphics.ellipse("fill", x, y, 2.5, 5)
-  end
-  if player.dir+14/180*math.pi < math.pi/2 and player.dir+14/180*math.pi > -math.pi/2 then
-    x,y,z = p3d({x=16, y=-20, z=0}, (player.dir)+14/180*math.pi)
-    love.graphics.ellipse("fill", x, y, 2.5, 5)
+  table.sort(player.objects, function(a,b) 
+    return (a.z < b.z)
+  end)
+  for i=1, #player.objects do
+    player.objects[i]:draw()
   end
   
   love.graphics.translate(-player.x, -(player.z-24+player.y))
