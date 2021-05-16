383e2c053bdbdda9ace091eba0639370d39f855b
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-05-16 12:30:50 +0800

    trees no longer depth sort

--- tree.lua
+++ tree.lua
@@ -1,10 +1,21 @@
 intro = require('intro') -- require intro for access to dependencies 
 local entity = {}
 
-function entity.new(treeX, treeZ)
-  local tree = {id = 'tree', x = treeX or 60, y = 00, z = treeY or 30}
+function entity.new(camera, treeX, treeZ)
+  local tree = {id = 'tree', camera = camera, x = treeX or 60, y = 0, z = treeZ or 30, shadow = 20*1.3}
 
-  tree.objects = { -- table of every body part in the tree (automatically gets sorted by z each frame)
+  local function p3d(p, rotation)
+    rotation = rotation or player.dir -- rotation is a scalar that rotates around the y axis
+
+    local x = math.sin(rotation)*p.x   + 0   +  math.sin(rotation+math.pi/2)*p.z
+    local y = math.cos(rotation)*p.x/2 + p.y +  math.cos(rotation+math.pi/2)*p.z/2
+
+    local z = math.cos(rotation)*p.x   - p.y +  math.cos(rotation+math.pi/2)*p.z
+
+    return x,y,z
+  end
+
+  tree.objects = { -- table of every section of the tree (automatically gets sorted by z each frame)
     {
       id='stump',
       z=1,
@@ -35,39 +46,40 @@ function entity.new(treeX, treeZ)
         self.z = 44
       end 
     },
-    {
-      id='shadowStump',
-      z=4,
-      draw = function(self)
-        love.graphics.setColour(0.53, 0.33, 0.29)
-        love.graphics.ellipse("fill", 0, -19, 10, 5)
+    -- {
+    --   id='shadowStump',
+    --   z=4,
+    --   draw = function(self)
+    --     love.graphics.setColour(0.53, 0.33, 0.29)
+    --     love.graphics.ellipse("fill", 0, -19, 10, 5)
 
-        self.z = 19
-      end 
-    },
-    {
-      id='shadowTrunk',
-      z=5,
-      draw = function(self)
-        love.graphics.setColour(0.53, 0.33, 0.29)
-        love.graphics.rectangle("fill", -10, -29, 20, 10)
+    --     self.z = 19
+    --   end 
+    -- },
+    -- {
+    --   id='shadowTrunk',
+    --   z=5,
+    --   draw = function(self)
+    --     love.graphics.setColour(0.53, 0.33, 0.29)
+    --     love.graphics.rectangle("fill", -10, -29, 20, 10)
 
-        self.z = 19
-      end 
-    },
+    --     self.z = 19
+    --   end 
+    -- },
   }
 
   function tree:draw()
-  	love.graphics.translate(self.x, self.z/2+self.y)
+    local tx, ty = p3d({x=self.z, y=self.y, z=self.x}, self.camera.r)
+    love.graphics.translate(tx, ty)
 
-    table.sort(self.objects, function(a,b) 
-      return (a.z < b.z)
-    end)
+    -- table.sort(self.objects, function(a,b) 
+    --   return (a.z < b.z)
+    -- end)
     for i=1, #self.objects do
       self.objects[i]:draw()
     end
     
-    love.graphics.translate(-self.x, -(self.z/2+self.y))
+    love.graphics.translate(-tx, -ty)
   end
 
   function tree:update()
