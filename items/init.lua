15419243eca0ace9360d6c281a83f98244030d53
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-05-20 13:30:41 +0800

    added wood

--- items/init.lua
+++ items/init.lua
@@ -1,4 +1,5 @@
 local items = {
-	{name='Sword', type='sword', img=love.graphics.newImage('items/sword.png')}
+	{name='Sword', type='sword', img=love.graphics.newImage('items/sword.png')},
+	{name='Wood', type='material', img=love.graphics.newImage('items/wood.png')},
 }
 return items
