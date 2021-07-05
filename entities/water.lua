e8cf99ada187d89c97577f70786278bfd4c6eecd
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-07-05 17:22:36 +0800

    water rotates with camera

--- entities/water.lua
+++ entities/water.lua
@@ -6,7 +6,7 @@ function entity.new(camera, x, y)
   
 
   local function p3d(p, rotation)
-    rotation = rotation or 0 -- rotation is a scalar that rotates around the y axis
+    rotation = rotation or water.camera.dir -- rotation is a scalar that rotates around the y axis
 
     local x = math.sin(rotation)*p.x   + 0   +  math.sin(rotation+math.pi/2)*p.z
     local y = math.cos(rotation)*p.x/2 + p.y +  math.cos(rotation+math.pi/2)*p.z/2
@@ -21,7 +21,13 @@ function entity.new(camera, x, y)
     love.graphics.translate(tx, ty)
 
     love.graphics.setColour(0.25,0.63,0.73)
-    love.graphics.rectangle("fill", -25, -25, 50, 25)
+    -- love.graphics.rectangle("fill", -25, -25, 50, 25)
+    local x,y = p3d({x=0-25, y=0, z=0-25})
+    local x1,y1 = p3d({x=0+25, y=0, z=0-25})
+    local x2,y2 = p3d({x=0+25, y=0, z=0+25})
+    local x3,y3 = p3d({x=0-25, y=0, z=0+25})
+    love.graphics.polygon("fill", x,y,x1,y1,x2,y2,x3,y3)
+
 
     love.graphics.translate(-tx, -ty)
   end
